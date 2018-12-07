e584fffde38acc5936f07b7a549b02aa257d0871
Jan Rodriguez <thejanmanfan@yahoo.com>
2018-12-07 07:20:30 +0000

    Added blogsbackend connection

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var bodyParser      = require("body-parser"),
 express             = require("express"),
-expressSanitizer     = require("express-sanitizer"),
+expressSanitizer    = require("express-sanitizer"),
 methodOverride      = require("method-override"),
 mongoose            = require("mongoose"),
 app                 = express();
@@ -12,9 +12,32 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
-app.get("/",function(req,res){
-    res.render("index");
+//MONGOOSE MODEL CONFIG
+var blogSchema= new mongoose.Schema({
+    title:String,
+    image:String,
+    body:String,
+    created: {type: Date, default: Date.now}    //Read this as having a type of Date that is already initialized with the default value of the time that it was created
 })
+var Blog=mongoose.model("Blog",blogSchema)
+
+
+
+
+app.get("/",function(req,res){
+    res.render("landing");
+});
+
+app.get("/index" , function(req,res){
+    Blog.find({}, function(err,blogs){
+        if(err){
+            console.log("error my nigga!");
+        }else{
+            res.render("index", {blogs:blogs});
+        }
+    })
+})
+
 
 
 app.listen(process.env.PORT,process.env.IP, function(){
