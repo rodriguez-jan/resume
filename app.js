5579559daed4b237976bdd159a44f9c576b8329b
Jan Rodriguez <thejanmanfan@yahoo.com>
2018-12-07 08:21:56 +0000

    Added full blogging functionality

--- app.js
+++ app.js
@@ -22,19 +22,83 @@ var blogSchema= new mongoose.Schema({
 var Blog=mongoose.model("Blog",blogSchema)
 
 
-
-
 app.get("/",function(req,res){
     res.render("landing");
 });
 
-app.get("/index" , function(req,res){
+//INDEX ROUTE
+app.get("/blogs", function(req,res){
     Blog.find({}, function(err,blogs){
         if(err){
-            console.log("error my nigga!");
+            console.log(err);
         }else{
-            res.render("index", {blogs:blogs});
+            res.render("index",{blogs:blogs});
         }
+    })
+})
+
+//NEW ROUTE
+app.get("/blogs/new", function(req,res){
+    res.render("new");
+})
+
+//CREATE ROUTE
+app.post("/blogs",function(req,res){
+    req.body.blog.body= req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(err,newBlog){
+        if(err){
+            res.render("new");
+        }else{
+            res.redirect("/blogs");
+        }
+    })
+})
+
+//SHOW ROUTE
+app.get("/blogs/:id",function(req,res){
+    Blog.findById(req.params.id, function(err,foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else {
+            res.render("show", {blog:foundBlog});
+        }
+    })
+})
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit",function(req,res){
+    Blog.findById(req.params.id, function(err,foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.render("edit", {blog:foundBlog});
+        }
+    })
+})
+
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req,res){
+    req.body.blog.body= req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updateBlog){
+        if(err){
+            res.redirect("/blogs");
+        
+        }else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req,res){
+    Blog.findByIdAndRemove(req.params.id,function(err){
+        if(err){
+            res.reidrect("/blogs");
+        }else{
+            res.redirect("blogs");
+        }
+        
     })
 })
 
